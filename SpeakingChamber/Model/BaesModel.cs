a4e777e3cbb29fbe7388fd79db4855e3c52ae4e1
huy bui dac <huybuidac@gmail.com>
2017-08-08 01:22:27 +0700

    Init Project

---
+++ SpeakingChamber/Model/BaesModel.cs
@@ -0,0 +1,13 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakingChamber.Model
+{
+    [AddINotifyPropertyChangedInterface]
+    abstract class BaesModel
+    {
+    }
+}
---
+++ SpeakingChamber/ViewModel/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PropertyChanged;
+
+namespace SpeakingChamber.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    abstract class BaseViewModel
+    {
+    }
+}
